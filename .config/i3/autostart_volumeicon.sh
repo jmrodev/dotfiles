4dba6ef5f2d3d7f13d1f356e2ac708198a1b581f
jmrodev <juanmarcelo.rodrigueztandil@gmail.com>
2025-06-17 17:08:01 -0300

    delete comments

--- .config/i3/autostart_volumeicon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Play a silent sound or a very short sound (you might need 'aplay' from alsa-utils)
-# aplay /usr/share/sounds/alsa/Front_Left.wav & # You might need to find a small sound file on your system
-sleep 2
-volumeicon &
--- .zshrc
+++ .zshrc
@@ -1,2 +1,10 @@
 source ~/.zsh_secrets
 source ~/.config/zsh/.zshrc
+
+# pnpm
+export PNPM_HOME="/home/jmro/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
